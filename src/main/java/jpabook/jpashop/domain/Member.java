a3306ef76f61b55caad60a2339f9288079c737a9
LimHyoJin <1004dlagywls@naver.com>
2023-05-11 16:38:06 +0900

    Entity Class 개발

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.criterion.Order;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+}
