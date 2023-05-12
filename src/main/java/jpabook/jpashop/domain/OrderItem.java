20978973b5ccccc216cb533ba85849703b09e52d
LimHyoJin <1004dlagywls@naver.com>
2023-05-12 11:25:03 +0900

    Entity Class 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,15 +1,19 @@
 package jpabook.jpashop.domain;
 
+import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
-@Getter@Setter
+@Getter
+@Setter
 public class OrderItem {
 
-    @Id@GeneratedValue@Column(name = "order_item_id")
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
     private Long id;
 
     private Item item;
