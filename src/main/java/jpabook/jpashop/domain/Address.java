46b50e3d8643e5b41436aafa840193f243e0fa98
LimHyoJin <1004dlagywls@naver.com>
2023-05-16 14:47:53 +0900

    Entity Class 개발2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -13,4 +13,15 @@ public class Address {
     private String street;
 
     private String zipcode;
+
+    //JPA 특성상 엔티티나 임베디드 타입 자바 기본 생성자는 public 보다 protected로 설정하는게 그나마 더 안전하다
+    //JPA 구현 라이브러리가 객체를 생성할 떄 리플렉션 같은 기술을 사용할 수 있도록 지원해야하기 때문이다.
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -25,4 +25,11 @@ public class Category {
             joinColumns = @JoinColumn(name = "category_id"),
             inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> items = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
 }
