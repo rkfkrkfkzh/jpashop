2cb082d8a2ab3e9905eac39441bc0980110e52c3
LimHyoJin <1004dlagywls@naver.com>
2023-05-23 10:50:04 +0900

    회원 기능 테스트

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -9,15 +9,15 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional(readOnly = true)
+@Transactional(readOnly = true) //조회만 하고 수정은 하지 않는다는 의미
 @RequiredArgsConstructor // final이나 @NonNull이 붙은 필드들을 갖는 생성자가 자동으로 생성 AllArgsConstructor보다 나은듯
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
     //회원가입
-    @Transactional(readOnly = false)
-    public Long join(Member member) {
+    @Transactional(readOnly = false) //회원 가입은 읽기 전용이 아닌 트랜잭션에서 실행
+    public Long join(Member member) { // 회원 객체를 저장하고 생성된 회원의 ID를 반환
 
         validateDuplicateMember(member); //중복 회원 검출
         memberRepository.save(member);
@@ -36,7 +36,7 @@ public class MemberService {
         return memberRepository.findAll();
     }
 
-    public Member fintOne(Long memberId) {
+    public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
     }
 }
