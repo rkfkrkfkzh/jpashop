3ce34a4fdaa8a45d8974c5f782fe44553d50a8f8
LimHyoJin <1004dlagywls@naver.com>
2023-05-25 16:58:04 +0900

    주문 서비스 개발 中

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Fetch;
 
@@ -16,6 +18,7 @@ import static javax.persistence.FetchType.LAZY;
 @Table(name = "orders")
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
     @Id
@@ -40,27 +43,27 @@ public class Order {
     private OrderStatus status; //주문상태 [ORDER, CANCEL]
 
     //연관관계 메서드
-    public void setMember(Member member){
+    public void setMember(Member member) {
         this.member = member;
         member.getOrders().add(this);
     }
 
-    public void addOrderItem(OrderItem orderItem){
+    public void addOrderItem(OrderItem orderItem) {
         orderItems.add(orderItem);
         orderItem.setOrder(this);
     }
 
-    public void setDelivery(Delivery delivery){
-        this.delivery =delivery;
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
         delivery.setOrder(this);
     }
 
     //생성 메서드
-    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
         Order order = new Order();
         order.setMember(member);
         order.setDelivery(delivery);
-        for (OrderItem orderItem : orderItems){
+        for (OrderItem orderItem : orderItems) {
             order.addOrderItem(orderItem);
         }
         order.setStatus(OrderStatus.ORDER);
@@ -72,12 +75,12 @@ public class Order {
     /*
     주문 취소
      */
-    public void cancel(){
-        if (delivery.getStatus()==Deliverystatus.COMP){
+    public void cancel() {
+        if (delivery.getStatus() == Deliverystatus.COMP) {
             throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
         }
         this.setStatus(OrderStatus.CANCEL);
-        for (OrderItem orderItem : orderItems){
+        for (OrderItem orderItem : orderItems) {
             orderItem.cancel();
         }
     }
@@ -86,11 +89,15 @@ public class Order {
     /*
     전체 주문 가격 조회
      */
-    public int getTotalPrice(){
-        int totalPrice =0;
-        for (OrderItem orderItem : orderItems){
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
             totalPrice += orderItem.getTotalPrice();
         }
         return totalPrice;
     }
+//    public int getTotalPrice(){
+//        return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
+//    }   //람다 스트림
+
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -11,6 +13,7 @@ import static javax.persistence.FetchType.LAZY;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id
@@ -29,8 +32,10 @@ public class OrderItem {
     private int orderPrice; //주문 가격
     private int count; //주문 수량
 
+//    protected OrderItem(){}  // @NoArgsConstructor(access = AccessLevel.PROTECTED)로 대체
+
     //생성 메서드
-    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
         OrderItem orderItem = new OrderItem();
         orderItem.setItem(item);
         orderItem.setOrderPrice(orderPrice);
