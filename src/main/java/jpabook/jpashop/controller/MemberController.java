2bd299f1cd55b2022aa21291b463928a4faec6de
LimHyoJin <1004dlagywls@naver.com>
2023-06-12 13:53:59 +0900

    회원 목록 조희

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -28,8 +29,8 @@ public class MemberController {
     public String create(@Valid MemberForm form, BindingResult result) {
 
 
-        if(result.hasErrors()){
-            return"members/createMemberForm";
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
         }
         Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
 
@@ -39,6 +40,13 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 
 }
